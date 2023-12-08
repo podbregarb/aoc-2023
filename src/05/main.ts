32b109d50a9ba16e4ba96f72644c8e59fb236539
Barbka Podbregar <barbka@nefta.io>
2023-12-08 01:27:45 +0100

    5th day 2nd part solution

--- src/05/main.ts
+++ src/05/main.ts
@@ -46,7 +46,75 @@ function mapTo(seeds: number[], mapping: string): number[] {
 }
 
 function solveSecondPart(input: string[]): number {
-    let sum = 0;
-    return sum;
+    // seeds: 79 14 55 13
+    // [79, 14, 55, 13]
+    let seedsList = input[0].split(':')[1].trim().split(' ').map(s => Number(s));
+    let seedRanges: [number, number][] = [];
+    for (let i = 0; i < seedsList.length; i += 2) {
+        seedRanges.push([seedsList[i], seedsList[i] + seedsList[i + 1]]);
+    }
+
+    for (let i = 1; i < input.length; i++) {
+        seedRanges = mapRangesTo(seedRanges, input[i]);
+    }
+    return Math.min(...seedRanges.map(range => range[0]));
 }
 
+function mapRangesTo(seeds: [number, number][], mapping: string): [number, number][] {
+    // mapping example
+    // seed-to-soil map:
+    // 50 98 2
+    // 52 50 48
+    const ranges = mapping.split(':')[1].trim().split('\n').map(s => s.split(' ').map(s => Number(s)));
+    for (let i = 0; i < seeds.length; i++) {
+        // Range goes from seeds[i][0] to seeds[i][1]
+        // Range needs to be split (if necessary) to ranges defined in ranges
+        // [seeds[i][0], seeds[i][1]]
+        for (let j = 0; j < ranges.length; j++) {
+            // [ranges[j][1], ranges[j][1] + ranges[j][2]]
+            // [sourceRange, sourceRange + rangeLength]
+            const sourceRange = ranges[j][1];
+            const rangeLength = ranges[j][2];
+            if (seeds[i][0] <= sourceRange && (sourceRange + rangeLength) < seeds[i][1]) {
+                seeds.splice(
+                    i,
+                    1,
+                    [seeds[i][0], sourceRange - 1],
+                    [sourceRange, sourceRange + rangeLength - 1],
+                    [sourceRange + rangeLength, seeds[i][1]]
+                )
+            } else if (seeds[i][0] < sourceRange && seeds[i][1] > sourceRange && seeds[i][1] <= (sourceRange + rangeLength)) {
+                seeds.splice(i, 1, [seeds[i][0], sourceRange - 1], [sourceRange, seeds[i][1]])
+            } else if (seeds[i][0] > sourceRange && seeds[i][0] < (sourceRange + rangeLength) && seeds[i][1] >= (sourceRange + rangeLength)) {
+                seeds.splice(
+                    i,
+                    1,
+                    [seeds[i][0], sourceRange + rangeLength - 1],
+                    [sourceRange + rangeLength, seeds[i][1]]
+                )
+            }
+        }
+    }
+    let mappedTo: [number, number][] = [];
+    for (let i = 0; i < seeds.length; i++) {
+        for (let j = 0; j < ranges.length; j++) {
+            const destinationRange = ranges[j][0];
+            const sourceRange = ranges[j][1];
+            const rangeLength = ranges[j][2];
+            if (sourceRange <= seeds[i][0] && seeds[i][1] < (sourceRange + rangeLength)) {
+                if (!mappedTo[i]) {
+                    mappedTo.push(
+                        [
+                            seeds[i][0] + (destinationRange - sourceRange),
+                            seeds[i][1] + (destinationRange - sourceRange)
+                        ]
+                    );
+                }
+            }
+        }
+        if (!mappedTo[i]) {
+            mappedTo.push(seeds[i]);
+        }
+    }
+    return mappedTo;
+}
